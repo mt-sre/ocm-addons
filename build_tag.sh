376aeff4b2247c6f6a29d6982a1b6848d673f8a6
Andrew Pantuso <apantuso@redhat.com>
2022-02-07 10:45:40 -0500

    updating build_tag to enable publishing

--- build_tag.sh
+++ build_tag.sh
@@ -5,4 +5,4 @@ set -exvo pipefail -o nounset
 IMAGE=ocm-addons-ci
 
 docker build -t ${IMAGE} -f Dockerfile.ci .
-docker run --rm -e "GITHUB_TOKEN=${GITHUB_TOKEN}" "${IMAGE}" release:snapshot
+docker run --rm -e "GITHUB_TOKEN=${GITHUB_TOKEN}" "${IMAGE}" release:full
