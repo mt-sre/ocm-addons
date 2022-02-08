c62ba1f56b829abdc874ad25a7f86fdb749a74fe
Andrew Pantuso <apantuso@redhat.com>
2022-02-07 19:16:37 -0500

    updating timeouts and tag login

--- build_tag.sh
+++ build_tag.sh
@@ -2,6 +2,8 @@
 
 set -exvo pipefail -o nounset
 
+docker login quay.io -u ${QUAY_USER} -p ${QUAY_TOKEN}
+
 IMAGE=ocm-addons-ci
 
 docker build -t ${IMAGE} -f Dockerfile.ci .
--- pr_check.sh
+++ pr_check.sh
@@ -7,4 +7,5 @@ docker login quay.io -u ${QUAY_USER} -p ${QUAY_TOKEN}
 IMAGE=ocm-addons-ci
 
 docker build -t ${IMAGE} -f Dockerfile.ci .
-docker run --rm ${IMAGE} check test
+docker run --rm ${IMAGE} -t 10m check
+docker run --rm ${IMAGE} -t 10m test
